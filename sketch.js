7c4f29d0edd3b83e62f76cbb472f296e36efb560
krishna <srikrishna.mungara@gmail.com>
2021-04-06 18:46:56 -0400

    funtion keypress

--- sketch.js
+++ sketch.js
@@ -75,3 +75,16 @@ function mouseDragged(){
 function mouseReleased(){
     slingshot.fly();
 }
+function keyPressed (){
+    if (keyCode===32){
+       slingshot.attach(bird.body) 
+    }
+}
+
+
+
+
+
+
+
+
